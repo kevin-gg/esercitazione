b7a1836e4079fc4506a7eace0ae930aef7989252
kevin trentini <trentinikevin6@gmail.com>
2021-09-24 23:47:16 +0200

    Add project files.

---
+++ Esercitazione1/Program.cs
@@ -0,0 +1,32 @@
+﻿using GestioneArray;
+using System;
+
+namespace ex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int dimensione = Gestione.DimensioneArray();
+            string[]matricola = new string[dimensione];
+            string[]materia = new string[dimensione];
+            int[]voto = new int[dimensione];
+
+            for (int i = 0; i < dimensione; i++)
+            {
+                Gestione.CaricaArray(matricola, "la matricola",i);
+                Gestione.CaricaArray(materia, "la materia",i);
+                Gestione.CaricaArray(voto, "il voto",i);
+            }
+            Console.WriteLine("La media di quale materia vuoi vedere?");
+            string m = Console.ReadLine();
+            int n = Gestione.Materia(m, materia);
+            int[] votimateria = new int[n];
+            Gestione.RiempiArray(votimateria, materia, m);
+            double media = Gestione.Media(votimateria, voto);
+            Console.WriteLine($"la media dei voti della materia {m} è {media}");
+
+
+        }
+    }
+}
---
+++ GestioneArray/Gestione.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace GestioneArray
+{
+    public class Gestione
+    {
+        public static int DimensioneArray()
+        {
+            Console.Write("quanti alunni vuoi inserire?");
+            int n = int.Parse(Console.ReadLine());
+            return n;
+        }
+        public static void CaricaArray(string[] a, string parola, int i)
+        {
+            Console.WriteLine("inserisci " + parola + " " + (i + 1));
+            a[i] = Console.ReadLine();
+        }
+        public static void CaricaArray(int[] a, string parola, int i)
+        {
+            Console.WriteLine("inserisci " + parola + " " + (i + 1));
+            a[i] = int.Parse(Console.ReadLine());
+        }
+        public static int Materia(string m, string[] materia)
+        {
+            int n = 0;
+            for (int i = 0; i < materia.Length; i++)
+            {
+                if (materia[i] == m.ToLower())
+                {
+                    n++;
+                }
+            }
+            return n;
+        }
+        public static void RiempiArray(int[] array, string[] materia, string m)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (materia[i] == m.ToLower())
+                {
+                    array[i] += i;
+                }
+            }
+        }
+        public static double Media(int[] array, int[] voti)
+        {
+            int somma = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                somma += voti[array[i]];
+            }
+            double media = somma / array.Length;
+            return media;
+        }
+
+    }
+}
